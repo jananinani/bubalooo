014fcab5ac8596fe43d102e4377ba541157b7101
jananinani <jananibtechitksr2@gmail.com>
2016-07-19 10:05:22 +0530

    Add files via upload

--- demo.java
+++ demo.java
@@ -1,17 +1,12 @@
-# bubalooopublic class demo {
 
+public class demo {
+
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
-       int a;
-       Scanner sc=new Scanner(System.in);
-       a=sc.nextInt();
-       if(a%2==0)
-    	   {
-    	   System.out.println("The number is even");
-    	   }
-       else
-       {
-    	   System.out.println("The number is odd");
-       }
-    	   }
+		// TODO Auto-generated method stub
+
+	}
 
 }
