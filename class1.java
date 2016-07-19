7b80956197d8e8d94322bf4819745e5a464eee15
jananinani <jananibtechitksr2@gmail.com>
2016-07-19 09:47:21 +0530

    Create class1.java

---
+++ class1.java
@@ -0,0 +1,23 @@
+# bubalooopublic class sample1 {
+public static void main(String[] args) {
+		if(args.length==0)
+		{
+			System.out.println("An integer with an argument is expected");
+		}
+			int a;
+			a=Integer.parseInt(args[0]);
+      if(a>0)
+		{
+			System.out.println("The number is positive");
+			}
+		else if(a==0)
+		{
+			System.out.println("The number is neither positive nor negative");
+		}
+		else
+		{
+			System.out.println("The number is negative");
+		}
+	}
+
+}
