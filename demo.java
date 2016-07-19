69b1650608eeff31f7037b0af38719f91638fdf4
jananinani <jananibtechitksr2@gmail.com>
2016-07-19 09:55:26 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,17 @@
+# bubalooopublic class demo {
+
+	public static void main(String[] args) {
+       int a;
+       Scanner sc=new Scanner(System.in);
+       a=sc.nextInt();
+       if(a%2==0)
+    	   {
+    	   System.out.println("The number is even");
+    	   }
+       else
+       {
+    	   System.out.println("The number is odd");
+       }
+    	   }
+
+}
